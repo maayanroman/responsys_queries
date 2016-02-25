c0de10aa31fa95e7ca579f0304483c4edd7b131d
jodiwei <jodi.wei@avant.com>
2016-02-25 16:14:22 -0600

    Exclude customers who expired less than 160 days

--- YYYYMMDD_expired_looping_T1.sql
+++ YYYYMMDD_expired_looping_T1.sql
@@ -59,6 +59,7 @@ FROM   (SELECT a.customer_id,
         WHERE  a.row_id = 1 
                AND a.status = 'cancelled' 
                AND Cast(a.apr_percentage AS DECIMAL(9, 4)) < 0.40 
+               AND Trunc( From_tz(Cast(a.status_date AS TIMESTAMP), 'utc')) <= Trunc(CURRENT_DATE) - 160
                AND ( Mod( ( ( Trunc(CURRENT_DATE) - Trunc( From_tz(Cast(a.status_date AS TIMESTAMP), 'utc') ) ) - 160 ), 140) >= 1 
                      AND Mod( ( ( Trunc(CURRENT_DATE) - Trunc( From_tz(Cast(a.status_date AS TIMESTAMP), 'utc') ) ) - 160 ), 140) < 14 ) 
        /*If remainder of ((# of days since their app expired - 160) / 140) falls between [1,14), email sent.*/
